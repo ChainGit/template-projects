415893923f31bdbde3fae8b5453f31f1ecda5bc7
Chain Qian <chainz@foxmail.com>
2017-09-14 14:33:20 +0800

    update

---
+++ template1/src/main/java/com/chain/project/common/exception/ErrorDetail.java
@@ -0,0 +1,45 @@
+package com.chain.project.common.exception;
+
+/**
+ * 错误详情
+ */
+public class ErrorDetail {
+
+    private int code;
+    private String msg;
+
+    public ErrorDetail() {
+        this.code = ErrorCode.DEFAULT;
+        this.msg = ErrorCode.getErrorMsg(code);
+    }
+
+    public ErrorDetail(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("ErrorDetail{");
+        sb.append("code=").append(code);
+        sb.append(", msg='").append(msg).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
